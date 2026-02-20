8f38e62591e64fd85d1e8a1a294d462adc4f920b
Ujjawal Golani <ujjawalgolani@Ujjawals-MacBook-Air.local>
2026-02-20 16:36:32 +0530

    health-check

--- aviso_core/settings.py
+++ aviso_core/settings.py
@@ -89,6 +89,26 @@ DATABASES = {
 }
 
 
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
